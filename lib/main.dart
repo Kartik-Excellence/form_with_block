93525bd6c8ed4d6d3211b7f9bc08afd0894fa22c
Kartik-Excellence <kartik@excellencetechnologies.info>
2020-10-22 19:06:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Bloc') ),
+        body: Center(child: Text('App HEre'),),
+      ),
+      
+    );
+  }
+}
